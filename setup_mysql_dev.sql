ec80e1f57e846a7edb6d5462168d4f529db9fb48
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-04-02 22:38:36 +0000

    Mass update

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- AirBnB clone v2 MySQL server preparation script
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO'hbnb_dev'@'localhost';
+
