3cc37677fefd1411b61e3d132cfe514be240b5ae
student1043 <55001682+student1043@users.noreply.github.com>
2020-04-12 21:00:09 +0100

    Create 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr//bin/env bash
+# making the needed directories
+if [ ! -x /usr/sbin/nginx ]; then
+        sudo apt-get -y update
+        sudo apt-get -y install nginx
+fi
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+fakehtml="this is fake yall"
+echo "$fakehtml" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+static="\\\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}"
+sudo sed -i "36i $static" /etc/nginx/sites-enabled/default
+sudo service nginx restart
