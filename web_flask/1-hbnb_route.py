41b82336053c6ece151af7a491f53da6c593fe31
student1043 <1043@holbertonschool.com>
2020-04-21 23:41:04 +0100

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
         return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
         return 'HBNB'
 
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
