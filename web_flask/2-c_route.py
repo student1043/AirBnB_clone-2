f04550d640f0e23affb42c629f4de82d617e19f7
student1043 <1043@holbertonschool.com>
2020-04-22 03:53:45 +0100

    Two more tasks added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C %s' % text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
